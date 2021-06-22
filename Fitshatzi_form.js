7f69026edbc38242b576bc5fef334a5f7111fbbd
Luthfy1 <85975206+Luthfy1900@users.noreply.github.com>
2021-06-21 23:32:36 -0500

    Added New Files

---
+++ Fitshatzi_form.js
@@ -0,0 +1,14 @@
+<<<<<<< HEAD
+function clickButton() {
+    window.open("Client Information.html");
+}
+function myReset() {
+    document.getElementsByClassName("eigth_set").reset();
+=======
+function clickButton() {
+    window.open("Client Information.html");
+}
+function myReset() {
+    document.getElementsByClassName("eigth_set").reset();
+>>>>>>> ab1e14cc53adb18d7a41711e1986be6be101d12c
+}
